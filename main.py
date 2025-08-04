2d83164a5de81e546ef74fdeaf73059d3fcfe23e
Nadelix <Nadelixx@gmail.com>
2025-08-04 14:23:53 +0200

    game now ends when you collide with asteroid

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 from constants import *
 from player import Player
@@ -28,6 +29,10 @@ def main():
         updatables.update(dt)
         for drawable in drawables:
             drawable.draw(screen)
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print('Game over!')
+                sys.exit()
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
